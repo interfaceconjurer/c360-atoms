c3326aa9c85b4c1ada724ab4f983cf0764218a80
Jordan L Wright <47576172+interfaceconjurer@users.noreply.github.com>
2022-02-09 16:36:40 -0500

    repo set up | importing c360 modules

---
+++ README.md
@@ -0,0 +1,9 @@
+# c360-atoms
+
+## Getting started
+
+```yarn start```
+Start the development server
+
+```yarn build```
+Build your project for production. Outputs to `dist`.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './app.css';
+
+export function App() {
+    return (
+     <div grid='true'>
+        <h1 type-style='display-1'>Hello world!</h1>
+    </div> 
+    )
+  }
